2cbae9c6e48d26eec378a32c5921f613d3660e42
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-17 00:12:17 +0300

    Adding basic things

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const app = express();
+
+// TODO:change db name
+mongoose
+  .connect("mongodb://127.0.0.1:27017/petstagram")
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("DB error", err.message));
+
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+
+app.use(routes);
+
+app.listen(3000, console.log("Server listen Port:3000!"));
