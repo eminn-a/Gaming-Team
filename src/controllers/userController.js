f8395062cb4fdbfb2dd7efcfba533397b2796794
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-10-17 00:23:55 +0300

    Modifyed register and login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
+const userManager = require("../managers/userManager");
 
 router.get("/login", (req, res) => {
   res.render("users/login");
+});
+
+router.post("/login", (req, res) => {
+  const loginData = req.body;
+  console.log(loginData);
+  res.redirect("/");
 });
 
 router.get("/register", (req, res) => {
